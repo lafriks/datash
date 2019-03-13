54b61fc7032ac7b51320545fdc86e3f191d98e6f
Rousan Ali <rousanali786@gmail.com>
2019-03-13 21:39:39 +0530

    Initial setup

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const outputDirectory = 'dist';
+const mode = process.env.NODE_ENV || 'development';
+
+module.exports = {
+  entry: ['babel-polyfill', './src/client/index.js'],
+  output: {
+    path: path.join(__dirname, outputDirectory),
+    filename: 'bundle.js'
+  },
+  mode,
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader'
+      }
+    },
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    },
+    {
+      test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 8192,
+          fallback: 'file-loader',
+          name: () => {
+            if (mode === 'development') {
+              return 'assets/[name].[ext]';
+            }
+
+            return 'assets/[hash].[ext]';
+          },
+        }
+      }
+    }
+    ]
+  },
+  resolve: {
+    extensions: ['.json', '.js', '.jsx']
+  },
+  devServer: {
+    port: 3000,
+    open: true,
+    proxy: {
+      '/api': 'http://localhost:8080'
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin([outputDirectory]),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      favicon: './public/favicon.ico'
+    })
+  ]
+};
