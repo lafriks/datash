cc96f6c1b05c5970efb23a219a68e49ba84e842c
Rousan Ali <rousanali786@gmail.com>
2019-03-20 22:35:45 +0530

    Provide my portfolio link in copyright

--- src/client/components/Footer/index.js
+++ src/client/components/Footer/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { Icon } from 'antd';
+import { Icon, Button } from 'antd';
 import './index.css';
 
 const Footer = () => (
   <div className="footer">
     <Icon type="copyright" size="small" />
     <span>2019</span>
-    <span style={{ marginLeft: 5 }}>Rousan Ali</span>
+    <span style={{ marginLeft: 5 }}>
+      <Button
+        className="copyright-link"
+        type="default"
+        href="https://rousan.io"
+        target="_blank"
+      >
+        Rousan Ali
+      </Button>
+    </span>
   </div>
 );
 
