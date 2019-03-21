2cd953b52e39a910cc6d24a2d99bdb2c188c2ef4
Rousan Ali <rousanali786@gmail.com>
2019-03-21 23:10:05 +0530

    Use to hash to bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   entry: ['babel-polyfill', './src/client/index.js'],
   output: {
     path: path.join(__dirname, outputDirectory),
-    filename: 'bundle.js',
+    filename: '[hash].bundle.js',
     globalObject: 'this',
     publicPath: '/'
   },
