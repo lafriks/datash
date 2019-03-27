8809d1eaf7c28b1980142bd0c13831fc9e3975d8
Rousan Ali <rousanali786@gmail.com>
2019-03-27 10:52:25 +0530

    Add Producthunt badge

--- src/client/components/Footer/index.js
+++ src/client/components/Footer/index.js
@@ -4,18 +4,33 @@ import './index.css';
 
 const Footer = () => (
   <div className="footer">
-    <Icon type="copyright" size="small" />
-    <span>2019</span>
-    <span style={{ marginLeft: 5 }}>
-      <Button
-        className="copyright-link"
-        type="default"
-        href="https://rousan.io"
+    <div style={{ marginBottom: 5 }}>
+      <a
+        href="https://www.producthunt.com/posts/datash?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-datash"
+        rel="noopener noreferrer"
         target="_blank"
       >
+        <img
+          src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=148864&theme=light"
+          alt="Datash - Send and receive files with end-to-end encryption | Product Hunt Embed"
+          style={{ width: 200 }}
+        />
+      </a>
+    </div>
+    <div>
+      <Icon type="copyright" size="small" />
+      <span>2019</span>
+      <span style={{ marginLeft: 5 }}>
+        <Button
+          className="copyright-link"
+          type="default"
+          href="https://rousan.io"
+          target="_blank"
+        >
         Rousan Ali
-      </Button>
-    </span>
+        </Button>
+      </span>
+    </div>
   </div>
 );
 
